12982bcbaece47c62a1fc14d18062b39e6e94005
takkkk04 <very.melon3456@icloud.com>
2025-12-14 14:53:34 +0900

    管理者入力画面

---
+++ admin/admin.js
@@ -0,0 +1,21 @@
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+        import {
+            getFirestore,
+            collection,
+            addDoc,
+        } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+
+        // Your web app's Firebase configuration
+        const firebaseConfig = {
+            apiKey: 
+            authDomain: "kaketoco-001.firebaseapp.com",
+            projectId: "kaketoco-001",
+            storageBucket: "kaketoco-001.firebasestorage.app",
+            messagingSenderId: "822926026136",
+            appId: "1:822926026136:web:6e809a33d66841f60cf7ed"
+        };
+
+        const app = initializeApp(firebaseConfig);
+        const db = getFirestore(app);
