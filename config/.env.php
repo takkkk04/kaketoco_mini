686b3a6bea8c6005c699ef7f3ff5e4b535e40a03
takkkk04 <very.melon3456@icloud.com>
2026-01-20 13:54:02 +0900

    Remove env and DS_Store from git tracking

--- config/.env.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    "db_name" => "kaketoco-mini_mvp_01_db",
-    "db_host" => "mysql3112.db.sakura.ne.jp",
-    "db_user" => "kaketoco-mini_mvp_01_db",
-    "db_pass" => "Katehwam-r7a",
-    "db_charset" => "utf8mb4",
-];
