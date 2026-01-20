91b70ab0171f2ffac96496e54268c797132163c7
takkkk04 <very.melon3456@icloud.com>
2026-01-20 13:33:38 +0900

    first commit

---
+++ config/.env.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    "db_name" => "kaketoco-mini_mvp_01_db",
+    "db_host" => "mysql3112.db.sakura.ne.jp",
+    "db_user" => "kaketoco-mini_mvp_01_db",
+    "db_pass" => "Katehwam-r7a",
+    "db_charset" => "utf8mb4",
+];
